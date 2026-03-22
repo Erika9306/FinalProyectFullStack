40968ae9551cca9fd8ca8b59550e7b0a9dea677c
Erika Bausyte <e.bausyte93@gmail.com>
2026-03-22 19:03:21 +0100

    button

--- Frontend/src/components/Button/Button.jsx
+++ Frontend/src/components/Button/Button.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Button.css'
 
-export const Button = ({text, onClick, className}) => {
+export const Button = ({type= "button", text, onClick, className}) => {
   return (
     <button className={className}
      onClick={onClick}
+     type={type}
      >
       {text}
     </button>
