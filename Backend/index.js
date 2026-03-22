ecce9e8fec102f7a7abc963d1c3b3324636c8ab1
Erika Bausyte <e.bausyte93@gmail.com>
2026-03-22 16:43:02 +0100

    ¨deployment¨

--- Backend/index.js
+++ Backend/index.js
@@ -20,11 +20,12 @@ const PORT = process.env.PORT || 3000;
 
 // Global middlewares
 
-app.use(cors({
-  origin: 'http://localhost:5173', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
+// app.use(cors({
+//   origin: 'http://localhost:5173', 
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
