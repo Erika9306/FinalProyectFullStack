a1fdb7527eed7c7cd427fa045a4b368b2de8fa85
Erika Bausyte <e.bausyte93@gmail.com>
2026-04-02 16:05:07 +0200

    admins table

--- Frontend/src/components/Admin/AdminPerson/AdminPerson.jsx
+++ Frontend/src/components/Admin/AdminPerson/AdminPerson.jsx
@@ -22,36 +22,26 @@ export const AdminPerson = React.memo(() => {
   }, []);
 
   return (
-    <div className="admin-person-container">
+    <div className="admins-container">
       <h3>🛡️ Administradores ({admin.length})</h3>
-      <ul className="admin-list">
-        {admin.map(adm => (
-          <li key={adm._id} className="admin-card">
-            <div className="admin-info">
-              <p><strong>{adm.name}</strong></p>
-              <p><small>{adm.email}</small></p>
-            </div>
-{/*             
-            <div className="admin-actions">
-              <Button 
-                text="👁️ Detalle" 
-                onClick={() => console.log("Detalle", adm._id)} 
-                className="button-primary" 
-              />
-              <Button 
-                text="✏️ Editar" 
-                onClick={() => console.log("Editar", adm._id)} 
-                className="button-secondary" 
-              />
-              <Button 
-                text="🗑️ Borrar" 
-                onClick={() => console.log("Borrar", adm._id)} 
-                className="button-danger" 
-              />
-            </div> */}
-              </li> 
-        ))}
-      </ul>
+      <div className="admin-list">
+        <table className="admin-table">
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Email</th>                        
+            </tr>
+          </thead>
+          <tbody>
+            {admin.map(adm => (
+              <tr key={adm._id}>
+                <td><strong>{adm.name}</strong></td>
+                <td>{adm.email}</td>                                
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 });
